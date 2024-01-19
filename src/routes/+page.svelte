a7ef543b1a7158a6a7c71c2f96b99457cbd40839
code-sanchu <a.ruben00001@gmail.com>
2024-01-19 16:54:17 +0000

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import { Picture } from '^components';
 	import { Contact, Heading, Projects, Section, Services } from '^pages/landing';
 	import { fade } from 'svelte/transition';
+	import { sineInOut } from 'svelte/easing';
 </script>
 
 <script lang="ts">
@@ -39,14 +40,17 @@
 		class="mt-md md:mt-[4.5rem] xl:mt-[6rem] 2xl:mt-[8rem] w-[46vw] sm:w-[36vw] xl:w-[38vw] max-w-[800px] min-h-[120px] aspect-[7/5] 2xl:aspect-[7/4] overflow-hidden"
 	>
 		<div class="relative w-full min-w-[240px] xs:min-w-[300px] xs/sm:min-w-[320px]">
-			<div class="absolute inset-0" style:opacity={!sectionReady?.main ? 0 : 1}>
+			<div
+				class="absolute inset-0 opacity-0 transition-opacity duration-[400ms] ease-in-out"
+				style:opacity={!sectionReady?.main ? 0 : 1}
+			>
 				<Picture data={image.art[1]} imageClass="absolute inset-0 object-cover" loading="eager" />
 			</div>
 		</div>
 	</div>
 
 	<div
-		class="mt-lg md:mt-[4.5rem] xl:mt-[7rem] flex justify-center opacity-0 transition-opacity duration-[400ms] ease-in"
+		class="mt-lg md:mt-[4.5rem] xl:mt-[7rem] flex justify-center opacity-0 transition-opacity duration-[400ms] ease-in-out"
 		style:opacity={!sectionReady?.main ? 0 : 1}
 		id="home-section"
 	>
@@ -54,14 +58,14 @@
 	</div>
 
 	<div
-		class="mt-2xl md:mt-[7.5rem] xl:mt-[12rem] 2xl:mt-[15rem] opacity-0 transition-opacity duration-[400ms] ease-in"
+		class="mt-2xl md:mt-[7.5rem] xl:mt-[12rem] 2xl:mt-[15rem] opacity-0 transition-opacity duration-[400ms] ease-in-out"
 		style:opacity={!sectionReady?.main ? 0 : 1}
 	>
 		<Section.Heading align="right" text="Projects" />
 	</div>
 
 	<div
-		class="mt-lg md:mt-xl lg:mt-[3.75rem] transition-opacity duration-[400ms] ease-in"
+		class="mt-lg md:mt-xl lg:mt-[3.75rem] transition-opacity duration-[400ms] ease-in-out"
 		style:opacity={!sectionReady?.main ? 0 : 1}
 		id="projects-section"
 	>
@@ -71,7 +75,7 @@
 	</div>
 
 	{#if sectionReady?.main}
-		<div transition:fade>
+		<div transition:fade={{ duration: 400, easing: sineInOut }}>
 			<div class="mt-2xl md:mt-3xl">
 				<Section.Heading align="left" text="Services" />
 			</div>
